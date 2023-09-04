84e29c4ea9c98999e554aa4fa93a5c52acec18fa
YuuXD <139762722+YuuXD26@users.noreply.github.com>
2023-09-04 23:57:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,39 @@ var navLinks = document.getElementById("nav-items");
             navLinks.style.right = "-300px";
         }
 
-        function showInstagramPopup() {
+        function showInstagramPopup(event) {
+            event.preventDefault();
             document.getElementById("instagramPopup").style.display = "block";
         }
         
         function hideInstagramPopup() {
             document.getElementById("instagramPopup").style.display = "none";
         }
+
+        function showFacebookPopup(event) {
+            event.preventDefault();
+            document.getElementById("facebookPopup").style.display = "block";
+        }
+
+        function hideFacebookPopup() {
+            document.getElementById("facebookPopup").style.display = "none";
+        }
+
+        function showGithubPopup(event) {
+            event.preventDefault();
+            document.getElementById("gitHubPopup").style.display = "block";
+        }
+
+        function hideGithubPopup() {
+            document.getElementById("gitHubPopup").style.display = "none";
+        }
+
+        function showGmailPopup(event) {
+            event.preventDefault();
+            document.getElementById("gmailPopup").style.display = "block";
+        }
+
+        function hideGmailPopup() {
+            document.getElementById("gmailPopup").style.display = "none";
+        }
+
